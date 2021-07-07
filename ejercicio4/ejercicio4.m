daab3d3b5758c79d313efbefb5be9334f808afa7
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-07-07 09:36:42 -0300

    Agrego informe y presentación. Fix ejercicio 4

--- ejercicio4/ejercicio4.m
+++ ejercicio4/ejercicio4.m
@@ -29,7 +29,7 @@ subplot(224);imhist(int8(B),201);title('Histograma de B');
 %% Segmento
 %Busco graficamente el rango de valores que los colores que deseamos segmentar
 
-R=(A>70);% Segmento objectos rojos
+R=(A>73);% Segmento objectos rojos
 V=(A<-56);% Segmento objectos verdes
 N=(L==0);% Segmento objectos negros
 AZ=(B<-69);% Segmento objectos azules
@@ -41,21 +41,13 @@ mapAZ=[1 1 1; 0 0 1];
 mapN=[1 1 1; 0 0 0];
 
 %Cuento cuantos elementos hay de cada color
-nR=bwlabel(R);
-nR=max(max(nR));
-
-nV=bwlabel(V);
-nV=max(max(nV));
-
-nAZ=bwlabel(AZ);
-nAZ=max(max(nAZ));
-
-nN=bwlabel(N);
-nN=max(max(nN));
+[LR,nR]=bwlabel(R);
+[LV,nV]=bwlabel(V);
+[LAZ,nAZ]=bwlabel(AZ);
+[LN,nN]=bwlabel(N);
 
 figure
 subplot(141);imshow(R,mapR); title([num2str(nR),' Objetos Rojos']);
 subplot(142);imshow(V,mapV); title([num2str(nV),' Objetos Verdes']);
 subplot(143);imshow(AZ,mapAZ); title([num2str(nAZ),' Objetos Azules']);
 subplot(144);imshow(N, mapN);title([num2str(nN),' Objetos Negros']);
-
